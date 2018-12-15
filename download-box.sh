d7784d338f1522d19a59dbcf5af4b82b3a869baa
Jonathan H. Wage <jonwage@gmail.com>
2018-12-15 01:02:02 +0000

    Update box to version 3.4

--- download-box.sh
+++ download-box.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -f box.phar ]; then
-    wget https://github.com/humbug/box/releases/download/3.0.0-alpha.5/box.phar -O box.phar
+    wget https://github.com/humbug/box/releases/download/3.4.0/box.phar -O box.phar
 fi
 
 
